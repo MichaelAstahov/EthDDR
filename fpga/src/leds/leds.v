de2ec2cb4301829efd9aed6301fd9dd96f5a41d4
Michael <astahovmichael@gmail.com>
2021-11-13 19:29:26 +0200

    add support for fpga's uart commands
    added leds & reset logic to support the commands sent from PC

--- fpga/src/leds/leds.v
+++ fpga/src/leds/leds.v
@@ -3,6 +3,7 @@ module leds
     input  wire         clk,
     input  wire         rst,
     input  wire [3:0]   sw,
+    input wire          software_ledson,
     output wire         led0_r,
     output wire         led0_g,
     output wire         led0_b,
@@ -21,28 +22,54 @@ module leds
     output wire         led7   
 );
 
-localparam CNT_40M = 28'd40_000_000 - 28'd1;
+localparam CNT_50M = 28'd50_000_000;
 
-reg  rst_led_reg;
-wire test_logic_wire0;
-wire test_logic_wire1;
-wire test_logic_wire2;
-wire test_logic_wire3;
+reg         rst_led_reg;
+reg [27:0]  clk_cnt_reg;
+reg         clk_led_reg;
+reg [3:0]   led_shift_reg;
+reg         led_shift_reg_dir;
+reg [27:0]  led_cnt_logic_reg;
 
 initial begin
-    rst_led_reg = 1'b0;
+    rst_led_reg                 = 1'b0;
+    clk_cnt_reg                 = 28'b0;
+    clk_led_reg                 = 1'b0;
+    led_shift_reg               = 4'b0;
+    led_shift_reg_dir           = 1'b0;
 end
 
-//! Test Logic
-test_logic test_logic_inst (
-    .clk     (clk),
-    .rst     (rst),
-    .sw      (sw[3:0]),
-    .o_lgc0  (test_logic_wire0),
-    .o_lgc1  (test_logic_wire1),
-    .o_lgc2  (test_logic_wire2)
-);
+always @(posedge clk) begin
+    if (rst) begin
+        led_cnt_logic_reg           <= 28'b0;
+        led_shift_reg               <= 4'b0;
+        led_shift_reg_dir           <= 1'b0;
+    end else begin
+        if (software_ledson) begin
+            if (led_shift_reg == 4'b1111) begin
+                led_shift_reg_dir <= 1'b1;
+            end else if (led_shift_reg == 4'b0) begin
+                led_shift_reg_dir <= 1'b0;
+            end
 
+            if (led_cnt_logic_reg == CNT_50M - 1) begin
+                led_cnt_logic_reg   <= 28'b0;
+                if (~led_shift_reg_dir) begin
+                    led_shift_reg <= {led_shift_reg[2:0], 1'b1};
+                end else begin
+                    led_shift_reg <= {led_shift_reg[2:0], 1'b0};
+                    // led_shift_reg <= {1'b0, led_shift_reg[3:1]};
+                end
+            end else begin
+                led_cnt_logic_reg   <= led_cnt_logic_reg + 1'b1;
+            end
+        end else begin
+            led_cnt_logic_reg   <= 28'b0;
+            led_shift_reg        <= 4'b0;
+            led_shift_reg_dir    <= 1'b0;
+        end
+    end
+end
 
 always @(posedge clk) begin
     if (rst) begin
@@ -52,13 +79,37 @@ always @(posedge clk) begin
     end
 end
 
+always @(posedge clk) begin
+    if (rst) begin
+        clk_cnt_reg   <= 28'b0;
+    end else begin
+        if (clk_cnt_reg == CNT_50M - 1) begin
+            clk_led_reg   <= ~clk_led_reg;
+            clk_cnt_reg   <= 28'b0;
+        end else begin
+            clk_cnt_reg   <= clk_cnt_reg + 1'b1;
+        end
+    end
+end
 
-assign led4 = test_logic_wire0;
-assign led5 = test_logic_wire1;
-assign led6 = test_logic_wire2;
+pwm rst_pwm_inst (
+    .clk        (clk),
+    .rst        (1'b0),
+    .en         (rst_led_reg),
+    .dutycycle  (11'b11010100100),
+    .out        (led3_r)
+);
+
+pwm clk_pwm_inst (
+    .clk        (clk),
+    .rst        (rst),
+    .en         (clk_led_reg),
+    .dutycycle  (11'b11110011110),
+    .out        (led0_g)
+);
 
 
-assign led3_r = rst_led_reg;
+assign {led4, led5, led6, led7} = led_shift_reg;
 
 
 endmodule
